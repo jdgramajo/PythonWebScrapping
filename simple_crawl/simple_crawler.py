96f2809f001c5aaaeef23bf11cd168c5f4a11172
Juan Diego Gramajo <juan.gramajo@xoom.com>
2016-02-19 17:01:40 -0600

    Adds another module to start crawling, which uses first_crawl#download.

---
+++ simple_crawl/simple_crawler.py
@@ -0,0 +1,11 @@
+import re
+from first_crawl import download
+
+def crawl_sitemap(url):
+    sitemap = download(url)
+    links = re.findall('<loc>(.*?)</loc>', sitemap)
+    for link in links:
+        html = download(link)
+
+if __name__ == '__main__':
+    crawl_sitemap('http://example.webscraping.com/sitemap.xml')
